d8507b24e1ed41660fee5a62083abc71e54df8e7
Jarek W <jarek@fsi.pl>
2013-12-04 10:52:29 +0100

    Implementacja kodu dla formularza kontaktowego - poprawki 1

--- spec/Szachuje/WebBundle/Form/ContactTypeSpec.php
+++ spec/Szachuje/WebBundle/Form/ContactTypeSpec.php
@@ -1,30 +1,41 @@
 <?php
 
-namespace Szachuje\WebBundle\Form;
+namespace spec\Szachuje\WebBundle\Form;
 
-use Symfony\Component\Form\AbstractType;
-use Symfony\Component\Form\FormBuilderInterface;
+use PhpSpec\ObjectBehavior;
+use Prophecy\Argument;
+use Symfony\Component\Form\FormBuilder;
 use Symfony\Component\Validator\Constraints;
 
-class ContactType extends AbstractType
+class ContactTypeSpec extends ObjectBehavior
 {
-    public function buildForm(FormBuilderInterface $builder, array $options)
+    public function it_is_initializable()
     {
-        $builder->setMethod('POST');
+        $this->shouldHaveType('Szachuje\WebBundle\Form\ContactType');
+    }
+
+    public function it_have_name()
+    {
+        $this->getName()->shouldReturn('contact');
+    }
+
+    public function it_add_fields_during_build_form(FormBuilder $builder)
+    {
+        $builder->setMethod('POST')->shouldBeCalled();
 
         $builder->add('first_name', 'text', array(
             'label' => 'contact_form.first_name',
             'constraints' => array(
                 new Constraints\Length(array('max' => 255)),
             ),
-        ));
+        ))->shouldBeCalled();
 
         $builder->add('last_name', 'text', array(
             'label' => 'contact_form.last_name',
             'constraints' => array(
                 new Constraints\Length(array('max' => 255)),
             ),
-        ));
+        ))->shouldBeCalled();
 
         $builder->add('email', 'email', array(
             'label' => 'contact_form.email',
@@ -33,29 +44,27 @@ class ContactType extends AbstractType
                 new Constraints\Length(array('max' => 255)),
                 new Constraints\Email(),
             ),
-        ));
+        ))->shouldBeCalled();
 
         $builder->add('phone', 'text', array(
             'label' => 'contact_form.phone',
             'constraints' => array(
                 new Constraints\Length(array('min' => 9, 'max' => 9)),
             ),
-        ));
+        ))->shouldBeCalled();
 
         $builder->add('content', 'textarea', array(
             'label' => 'contact_form.content',
             'constraints' => array(
                 new Constraints\NotBlank(),
             ),
-        ));
+        ))->shouldBeCalled();
 
         $builder->add('submit', 'submit', array(
             'label' => 'contact_form.submit',
-        ));
+        ))->shouldBeCalled();
+
+        $this->buildForm($builder, array());
     }
 
-    public function getName()
-    {
-        return 'contact';
-    }
 }
