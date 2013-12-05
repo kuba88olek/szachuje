d7b69c255faf77f908ed4c7e7aac86ff35bbd69c
Jarek W <jarek@fsi.pl>
2013-12-05 16:01:40 +0100

    Testy dla formularza kontaktowego - poprawki 4

--- spec/Szachuje/WebBundle/Controller/PageControllerSpec.php
+++ spec/Szachuje/WebBundle/Controller/PageControllerSpec.php
@@ -5,6 +5,7 @@ namespace spec\Szachuje\WebBundle\Controller;
 use Doctrine\ORM\EntityRepository;
 use PhpSpec\ObjectBehavior;
 use Doctrine\ORM\EntityManager;
+use Symfony\Bundle\FrameworkBundle\Routing\Router;
 use Symfony\Component\Form\Form;
 use Symfony\Component\Form\FormFactory;
 use Symfony\Component\Form\FormView;
@@ -17,10 +18,10 @@ use Prophecy\Argument;
 
 class PageControllerSpec extends ObjectBehavior
 {
-    public function let(EntityManager $em, FormFactory $formFactory,
-        TwigEngine $templating, Translator $translator, \Swift_Mailer $mailer)
+    public function let(EntityManager $em, FormFactory $formFactory, TwigEngine $templating, Translator $translator,
+        \Swift_Mailer $mailer, Router $router)
     {
-        $this->beConstructedWith($em, $formFactory, $templating, $translator, $mailer);
+        $this->beConstructedWith($em, $formFactory, $templating, $translator, $mailer, $router);
     }
 
     public function it_should_render_homepage(EntityManager $em, TwigEngine $templating, EntityRepository $repository,
@@ -61,6 +62,54 @@ class PageControllerSpec extends ObjectBehavior
             ->willReturn(new Response());
 
         $this->contactAction($request)->shouldHaveType('Symfony\Component\HttpFoundation\Response');
+    }
+
+    public function it_should_accept_only_post(Request $request)
+    {
+        $request->isMethod('POST')->willReturn(false);
+
+        $this->shouldThrow('Symfony\Component\HttpKernel\Exception\NotFoundHttpException')
+            ->duringContactFormHandleAction($request);
+    }
+
+    public function it_should_not_send_email_on_invalid_data(FormFactory $formFactory, Form $form, Request $request,
+        \Swift_Mailer $mailer, Router $router)
+    {
+        $formFactory->create(Argument::type('Szachuje\WebBundle\Form\ContactType'))->willReturn($form);
+        $request->isMethod('POST')->willReturn(true);
+        $form->handleRequest($request)->shouldBeCalled();
+        $form->isValid()->shouldBeCalled()->willReturn(false);
+
+        $mailer->send(Argument::type('\Swift_Message'))
+            ->shouldNotBeCalled();
+
+        $router->generate(Argument::type('string'))->willReturn('/sample_path');
+
+        $this->contactFormHandleAction($request)->shouldHaveType('Symfony\Component\HttpFoundation\RedirectResponse');
+    }
+
+    public function it_should_send_email(FormFactory $formFactory, Form $form, Request $request,
+         \Swift_Mailer $mailer, Router $router)
+    {
+        $formFactory->create(Argument::type('Szachuje\WebBundle\Form\ContactType'))->willReturn($form);
+        $request->isMethod('POST')->willReturn(true);
+        $form->handleRequest($request)->shouldBeCalled();
+        $form->isValid()->shouldBeCalled()->willReturn(true);
+
+        $form->getData()->willReturn(array(
+            'first_name' => 'Jan',
+            'last_name' => 'Kowalski',
+            'email' => 'jan@example.com',
+            'phone' => '123456789',
+            'content' => 'Lorem Ipsum',
+        ));
+
+        $mailer->send(Argument::type('\Swift_Message'))
+            ->shouldBeCalled();
+
+        $router->generate(Argument::type('string'))->willReturn('/sample_path');
+
+        $this->contactFormHandleAction($request)->shouldHaveType('Symfony\Component\HttpFoundation\RedirectResponse');
     }
 
 }
